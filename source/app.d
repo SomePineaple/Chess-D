e1a2144300545e2dd5f7d1774ad91789d9e63a66
SomePineaple <njweb@protonmail.com>
2022-02-09 13:35:44 -0500

    ok it workie

--- source/app.d
+++ source/app.d
@@ -1,7 +1,7 @@
 import std.stdio;
 import core.stdc.stdlib;
 import engine.board;
-import bindbd.sdl;
+import bindbc.sdl;
 
 void main() {
     auto board = new Board();
@@ -9,7 +9,7 @@ void main() {
 
     auto ret = loadSDL();
     if (ret != sdlSupport) {
-        if (ret == SDLSupport.noLibary) {
+        if (ret == SDLSupport.noLibrary) {
             stderr.writeln("Could not find SDL library, please install it, and make sure its in your PATH");
         } else if (ret == SDLSupport.badLibrary) {
             stderr.writeln("Found a SDL library, but it won't work. Sorry. Try re-installing I guess.");
