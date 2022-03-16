ff3a48e3abfaaae65062d612df7c051b310bb74a
SomePineaple <njweb@protonmail.com>
2022-03-16 10:07:55 -0400

    queen moves

--- source/engine/pieces/queen.d
+++ source/engine/pieces/queen.d
@@ -16,6 +16,33 @@ class Queen : Piece {
     override Move[] getLegalMoves(Board board) {
         Move[] legalMoves;
 
+        int[] moveDirections = [1, -1, 8, -8, 7, -7, 9, -9];
+        foreach (direction; moveDirections) {
+            bool mustStop = false;
+            int currentPos = pos;
+            while (!mustStop) {
+                if (((direction == 7 || direction == -9 || direction == -1) && Board.lowColCheck(currentPos)) || 
+                    ((direction == -7 || direction == 9 || direction == 1) && Board.highColCheck(currentPos))) {
+                    mustStop = true;
+                    continue;
+                }
+                currentPos += direction;
+                Piece pieceOnNewPos = board.getPiece(currentPos);
+                if (pieceOnNewPos.getType() == PieceType.EMPTYSPACE)
+                    legalMoves ~= new Move(pos, currentPos, MoveType.NORMAL);
+                else if (pieceOnNewPos.getAlliance() != Alliance.NOPIECE) {
+                    if (pieceOnNewPos.getAlliance() == alliance)
+                        mustStop = true;
+                    else {
+                        legalMoves ~= new Move(pos, currentPos, MoveType.ATTACK);
+                        mustStop = true;
+                    }
+                } else {
+                    mustStop = true;
+                }
+            }
+        }
+
         return legalMoves;
     }
 }
